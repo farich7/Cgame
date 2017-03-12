b61bbd140a8d1e9c1d5613fc5d1873998337bbfd
Ho-Ren <horenkan50@gmail.com>
2017-03-11 16:52:43 -0800

    add heroes vs villains

--- main.cpp
+++ main.cpp
@@ -30,28 +30,40 @@ int main() {
     srand(seed);
 
     vector<Character *> adventurers;
-    adventurers.push_back(new Warrior("Arthur", MAX_HEALTH, 5, "King George"));
-    adventurers.push_back(new Warrior("Jane", MAX_HEALTH, 6, "King George"));
+    vector<Character *> villains;
+    adventurers.push_back(new Warrior("Jason", MAX_HEALTH, 5, "King George"));
+    adventurers.push_back(new Warrior("Heracles", MAX_HEALTH, 6, "King George"));
     adventurers.push_back(new Warrior("Bob", MAX_HEALTH, 4, "Queen Emily"));
-    adventurers.push_back(new Elf("Raegron", MAX_HEALTH, 4, "Sylvarian"));
-    adventurers.push_back(new Elf("Cereasstar", MAX_HEALTH, 3, "Sylvarian"));
-    adventurers.push_back(new Elf("Melimion", MAX_HEALTH, 4, "Valinorian"));
-    adventurers.push_back(new Wizard("Merlin", MAX_HEALTH, 5, 10));
-    adventurers.push_back(new Wizard("Adali", MAX_HEALTH, 5, 8));
-    adventurers.push_back(new Wizard("Vrydore", MAX_HEALTH, 4, 6));
+    // adventurers.push_back(new Elf("Raegron", MAX_HEALTH, 4, "Sylvarian"));
+    // adventurers.push_back(new Elf("Cereasstar", MAX_HEALTH, 3, "Sylvarian"));
+    // adventurers.push_back(new Elf("Melimion", MAX_HEALTH, 4, "Valinorian"));
+    // adventurers.push_back(new Wizard("Merlin", MAX_HEALTH, 5, 10));
+    // adventurers.push_back(new Wizard("Adali", MAX_HEALTH, 5, 8));
+    // adventurers.push_back(new Wizard("Vrydore", MAX_HEALTH, 4, 6));
+    villains.push_back(new Wizard("Merlin", MAX_HEALTH, 5, 10));
+    villains.push_back(new Wizard("Adali", MAX_HEALTH, 5, 8));
+    villains.push_back(new Wizard("Vrydore", MAX_HEALTH, 4, 6));
 
     unsigned numAttacks = 10 + rand() % 11;
     unsigned attacker, defender;
     for (unsigned i = 0; i < numAttacks; ++i) {
         attacker = rand() % adventurers.size();
-        do {
-            defender = rand() % adventurers.size();
-        } while (defender == attacker);
+       // do {
+           
+        defender = rand() % villains.size();
+            //cannot attack yourself
+        // } while (defender == attacker); 
+        adventurers.at(attacker)->attack(*villains.at(defender));
 
-        adventurers.at(attacker)->attack(*adventurers.at(defender));
+        attacker = rand() % adventurers.size();
+        defender = rand() % villains.size();
+        villains.at(defender)->attack(*adventurers.at(attacker));
         cout << endl;
     }
+    cout << "HEROES: ";
     printHealth(adventurers);
-    while(true)
-    {}
+    cout << "VILLAIN: ";
+    printHealth(villains);
+    char END;
+    cin >> END;
 }
