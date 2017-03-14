5fab297032e23f9b45a5e4d9c158e6fceeb3ae06
Ho-Ren <horenkan50@gmail.com>
2017-03-14 02:07:04 -0700

    Accept more inputs for restart

--- main.cpp
+++ main.cpp
@@ -53,9 +53,9 @@ int main() {
     // adventurers.push_back(new Wizard("Merlin", MAX_HEALTH, 5, 10));
     // adventurers.push_back(new Wizard("Adali", MAX_HEALTH, 5, 8));
     // adventurers.push_back(new Wizard("Vrydore", MAX_HEALTH, 4, 6));
-    villains.push_back(new Wizard("Merlin", MAX_HEALTH, 5, 10));
-    villains.push_back(new Wizard("Adali", MAX_HEALTH, 5, 8));
-    villains.push_back(new Wizard("Vrydore", MAX_HEALTH, 4, 6));
+    villains.push_back(new Wizard("Aello", MAX_HEALTH, 5, 10));
+    villains.push_back(new Wizard("Ocypete", MAX_HEALTH, 5, 8));
+    villains.push_back(new Wizard("Celaeno", MAX_HEALTH, 4, 6));
 
 
     cout << "WELCOME TO JASON AND THE ARGONAUTS" << endl;
@@ -70,38 +70,49 @@ int main() {
         {
             villains.at(i)->setHealth(MAX_HEALTH);
         }
+
         do 
         {
-
-            unsigned numAttacks = 3;
+            unsigned numAttacks = adventurers.size();
             unsigned attacker, defender;
             for (unsigned i = 0; i < numAttacks; ++i) {
                 attacker = rand() % adventurers.size();
             // do {
-                
                 defender = rand() % villains.size();
                     //cannot attack yourself
                 // } while (defender == attacker); 
+                while(adventurers.at(attacker)->getHealth() == 0)
+                {
+                    attacker = rand() % villains.size();
+                }
                 adventurers.at(attacker)->attack(*villains.at(defender));
+                if(partyDead(adventurers) || partyDead(villains))
+                { break;}
 
                 attacker = rand() % adventurers.size();
                 defender = rand() % villains.size();
+                while(villains.at(defender)->getHealth() == 0)
+                {
+                    defender = rand() % villains.size();
+                }
                 villains.at(defender)->attack(*adventurers.at(attacker));
+                if(partyDead(adventurers) || partyDead(villains))
+                { break;}
                 cout << endl;
             }
             cout << "HEROES: ";
             printHealth(adventurers);
             cout << endl;
-            cout << "VILLAIN: ";
+            cout << "VILLAINS: ";
             printHealth(villains);
-
-            if(partyDead(adventurers))
+            if(partyDead(adventurers) || partyDead(villains))
             {
                 break;
             }
+            cout << "Press 'a' to ATTACK" << endl;
             char END;
             cin >> END;
-        } while(!partyDead(adventurers));
+        } while((!partyDead(villains)) && (!partyDead(adventurers)));
         cout << endl;
         if(partyDead(adventurers))
         {
@@ -109,5 +120,5 @@ int main() {
         }
         cout << "Press R to play again(anything else to stop)!" << endl;
         cin >> in;
-    } while(in == 'R');
+    } while(in == 'R' || in == 'r');
 }
