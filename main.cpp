b13b4146cb056b77bab07d7d5cf465857829935e
Ho-Ren <horenkan50@gmail.com>
2017-03-13 21:29:33 -0700

    Game runs until party is dead

--- main.cpp
+++ main.cpp
@@ -20,6 +20,17 @@ void printHealth(const vector<Character *> &adventurers)
     }
 }
 
+bool partyDead(const vector<Character *> &party)
+{
+    for(unsigned i = 0; i < party.size(); ++i)
+    {
+        if(party.at(i)->getHealth() > 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main() {
     int seed;
@@ -44,26 +55,33 @@ int main() {
     villains.push_back(new Wizard("Adali", MAX_HEALTH, 5, 8));
     villains.push_back(new Wizard("Vrydore", MAX_HEALTH, 4, 6));
 
-    unsigned numAttacks = 10 + rand() % 11;
-    unsigned attacker, defender;
-    for (unsigned i = 0; i < numAttacks; ++i) {
-        attacker = rand() % adventurers.size();
-       // do {
-           
-        defender = rand() % villains.size();
-            //cannot attack yourself
-        // } while (defender == attacker); 
-        adventurers.at(attacker)->attack(*villains.at(defender));
+    do 
+    {
 
-        attacker = rand() % adventurers.size();
-        defender = rand() % villains.size();
-        villains.at(defender)->attack(*adventurers.at(attacker));
+    
+        unsigned numAttacks = 10 + rand() % 11;
+        unsigned attacker, defender;
+        for (unsigned i = 0; i < numAttacks; ++i) {
+            attacker = rand() % adventurers.size();
+        // do {
+            
+            defender = rand() % villains.size();
+                //cannot attack yourself
+            // } while (defender == attacker); 
+            adventurers.at(attacker)->attack(*villains.at(defender));
+
+            attacker = rand() % adventurers.size();
+            defender = rand() % villains.size();
+            villains.at(defender)->attack(*adventurers.at(attacker));
+            cout << endl;
+        }
+        cout << "HEROES: ";
+        printHealth(adventurers);
         cout << endl;
-    }
-    cout << "HEROES: ";
-    printHealth(adventurers);
-    cout << "VILLAIN: ";
-    printHealth(villains);
-    char END;
-    cin >> END;
+        cout << "VILLAIN: ";
+        printHealth(villains);
+
+        char END;
+        cin >> END;
+    } while(!partyDead(adventurers));
 }
